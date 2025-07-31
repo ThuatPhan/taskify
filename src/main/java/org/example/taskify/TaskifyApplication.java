84a48e27086b646aa14d3cdee65761e941978e4c
Thuat Phan <thuatphan761@gmail.com>
2025-07-31 14:45:25 +0700

    Initial commit

---
+++ src/main/java/org/example/taskify/TaskifyApplication.java
@@ -0,0 +1,13 @@
+package org.example.taskify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/taskify/TaskifyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.taskify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
